b6ecef18c3a9666e40b4253dd4aff1ced6baa558
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-09-30 21:10:25 -0400

    Added a functioning map, need to connect navigation to the foooter and update with user location

--- views/mapView/mapView.js
+++ views/mapView/mapView.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import MapView from 'react-native-maps';
+
+import Footer from '../../components/footer/footer';
 
 const mapView = () => {
-    const currentLocation = Geolocation.getCurrentPosition((info => console.log("latitude: " + info.coords.latitude + " longitude: " + info.coords.longitude)));
+
+    
+
+    const [region, setRegion] = React.useState({
+        latitude: 37.3230,
+        longitude: -122.0322,
+        latitudeDelta: 0.0922, 
+        longitudeDelta: 0.0421
+    });
+
+    Geolocation.getCurrentPosition((info =>  {
+
+        setRegion({
+            latitude: info.coords.latitude,
+            longitude: info.coords.longitude,
+            latitudeDelta: region.latitudeDelta,
+            longitudeDelta: region.longitudeDelta
+        });
+        
+    }));
+   
     
     return(
-        <View>
-            <Text>Location Console Log</Text>
+        <View style={styles.mapContainer}>
+            <MapView 
+                initialRegion={region}
+                onRegionChange={setRegion}
+                style={styles.map}
+            />
+            <Footer style={{alignSelf: 'flex-end'}}/>
         </View>
+            
     );
+
+    
 }
+const styles ={
+    mapContainer: {
+        flex: 1
+    },
+    map: {
+        flex: 1,
+        width: '100%',
+        marginTop: '5%',
+        alignSelf: 'center',
+        justifyContent: 'center'
+    }
+};
 
 export default mapView;
